57d556558edcd68682fc01a64454ff127f9d06ec
Eaulmesse <theo.guerin01120@gmail.com>
2025-04-04 12:35:05 +0200

    add: Delete/Put/FindById for contact

--- src/routes/contacts.ts
+++ src/routes/contacts.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express';
-import { Db } from 'mongodb';
+import { Db, ObjectId } from 'mongodb';
 import { Contact, validateContact } from '../models/contact'; // Assurez-vous que le modèle Contact est correctement importé
 
 export default  (db: Db) => {
@@ -40,6 +40,66 @@ export default  (db: Db) => {
             res.status(500).send('Erreur interne du serveur.');
         }
     });
+
+    // Route GET pour récupérer un contact par ID
+    router.get('/contacts/:id', async (req: Request, res: Response) => {
+        try {
+            const contactId = req.params.id;
+            const contact = await db.collection('contacts').findOne({ _id: new ObjectId(contactId) });
+            if (!contact) {
+                res.status(404).send('Contact non trouvé.');
+                return;
+            }
+            res.status(200).send(contact);
+        }
+        catch (error) { 
+            console.error('Erreur lors de la récupération du contact:', error);
+            res.status(500).send('Erreur interne du serveur.');
+        }
+    });
+    
+
+    // Route DELETE pour supprimer un contact par ID
+    router.delete('/contacts/:id', async (req: Request, res: Response) => {
+        try {
+            const contactId = req.params.id;
+            const result = await db.collection('contacts').deleteOne({ _id: new ObjectId(contactId) });
+            if (result.deletedCount === 0) {
+                res.status(404).send('Contact non trouvé.');
+                return;
+            }
+            res.status(200).send('Contact supprimé avec succès!');
+        } catch (error) {
+            console.error('Erreur lors de la suppression du contact:', error);
+            res.status(500).send('Erreur interne du serveur.');
+        }
+    });
+
+    //  Route UPDATE pour mettre à jour un contact par ID
+    router.put('/contacts/:id', async (req: Request, res: Response) => {
+        const contactId = req.params.id;
+        const updatedContact: Partial<Contact> = req.body; 
+        const validationError = validateContact(updatedContact);
+        
+        if (validationError) {
+            res.status(400).send({ error: validationError });
+            return;
+        }
+        
+        // Insertion en BDD
+        try {
+            const result = await db.collection('contacts').updateOne({ _id: new ObjectId(contactId)}, {$set: updatedContact});
+            if (result.matchedCount === 0) {
+                res.status(404).send('Contact non trouvé.');
+                return;
+            }
+            res.status(200).send({ message: 'Contact mis à jour avec succès!' });
+        }
+        catch (error) {
+            console.error('Erreur lors de l\'insertion dans MongoDB:', error);
+            res.status(500).send('Erreur interne du serveur lors de l update.');
+        }
+    });    
 
     return router;
 };
